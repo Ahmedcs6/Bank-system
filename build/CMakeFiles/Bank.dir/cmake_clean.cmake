a477781e356b0fcb6f18bd3199de1fec6ee0b844
AhmedCs6 <ahmedmahmoudmos@gmail.com>
2025-06-15 15:13:53 +0300

    Extemsion 1

---
+++ build/CMakeFiles/Bank.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bank.dir/link.d"
+  "Bank"
+  "Bank.pdb"
+  "CMakeFiles/Bank.dir/src/main.cpp.o"
+  "CMakeFiles/Bank.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
